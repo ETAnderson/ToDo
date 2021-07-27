df61de877310ca9af743b73471b7ef6831dd7014
Eric Anderson <anderson.eric.t@gmail.com>
2021-07-27 14:42:59 -0400

    extracted and ignored secret key

---
+++ SECRETKEY.py
@@ -0,0 +1,1 @@
+secretkey = 'ml8^(&q5z3k!e%9o=acr*vwxtv-l)-9nj6air0klqtqzy^5c(9'
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+from SECRETKEY import secretkey
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=g)&eh7)=ri!w4=i$we0e#m6t68(j&3n6kgfxuq66nuc2(h7m5'
+SECRET_KEY = secretkey
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
